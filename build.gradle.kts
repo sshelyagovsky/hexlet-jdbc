8b013a0e8b032e11182b861d4c92b6ea75d2c183
s.shelyagovsky <s.shelyagovsky@gmail.com>
2024-06-22 21:05:23 +0300

    jbdc common princips

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
+    application
     id("java")
     id("checkstyle")
+}
+
+application {
+    mainClass = "hexlet.code.Application"
 }
 
 group = "hexlet.code"
@@ -13,8 +18,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.h2database:h2:2.2.220")
 }
 
 tasks.test {
     useJUnitPlatform()
-}gi
+}
