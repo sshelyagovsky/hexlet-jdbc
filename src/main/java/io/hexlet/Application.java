554bf4e224850b758bcd7c7dc953fb23040a2832
s.shelyagovsky <s.shelyagovsky@gmail.com>
2024-06-23 10:43:15 +0300

    try-with-resources

--- src/main/java/io/hexlet/Application.java
+++ src/main/java/io/hexlet/Application.java
@@ -4,37 +4,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Application {
-    // Нужно указывать базовое исключение,
-    // потому что выполнение запросов может привести к исключениям
+
     public static void main(String[] args) throws SQLException {
-        // Создаем соединение с базой в памяти
-        // База создается прямо во время выполнения этой строчки
-        // Здесь hexlet_test — это имя базы данных
-        var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
-        var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
-        // Чтобы выполнить запрос, создадим объект statement
-        var statement = conn.createStatement();
-        statement.execute(sql);
-        statement.close();
 
-        var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-        var statement2 = conn.createStatement();
-        statement2.executeUpdate(sql2);
-        statement2.close();
+        // Соединение с базой данных тоже нужно отслеживать
+        try (var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
 
-        var sql3 = "SELECT * FROM users";
-        var statement3 = conn.createStatement();
-        var resultSet = statement3.executeQuery(sql3);
+            var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            try (var statement = conn.createStatement()) {
+                statement.execute(sql);
+            }
 
-        // Набор данных — это итератор
-        // Мы перемещаемся по нему с помощью next() и каждый раз получаем новые значения
+            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
+            try (var statement2 = conn.createStatement()) {
+                statement2.executeUpdate(sql2);
+            }
 
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("username"));
-            System.out.println(resultSet.getString("phone"));
+            var sql3 = "SELECT * FROM users";
+            try (var statement3 = conn.createStatement()) {
+                var resultSet = statement3.executeQuery(sql3);
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("username"));
+                    System.out.println(resultSet.getString("phone"));
+                }
+            }
+            conn.close();
         }
-        statement3.close();
-        // Закрываем соединение
-        conn.close();
     }
 }
